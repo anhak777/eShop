272b9f8c0e2de7fed6ab36a911b541ce4468a167
derenik_javadyan <derenjavadyan@gmail.com>
2022-03-31 19:26:14 +0400

    contact us page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -50,7 +51,8 @@ import { FooterComponent } from './home/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  contactUs = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.email, Validators.required]),
+    message: new FormControl('', Validators.required)
+  })
+
+  get name(){
+    return this.contactUs.get('name')
+  }
+
+  get email(){
+    return this.contactUs.get('email')
+  }
+
+  get message(){
+    return this.contactUs.get('message')
+  }
+
+  onSubmit(){
+    console.log(this.contactUs.value) 
+  }
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  
 
 }
